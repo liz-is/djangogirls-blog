d11031a891a320a9efbae6179c392cb6ec13f8a7
Liz Ing-Simmons <liz.ing-simmmons@kcl.ac.uk>
2024-03-20 14:08:48 +0000

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = "blog"
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path("<int:post_id>/", views.detail, name="detail"),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:post_id>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:post_id>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:post_id>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:post_id>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:post_id>/remove/', views.comment_remove, name='comment_remove'),
+]
---
+++ blog/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+# Create your views here.
+
+def post_list(request):
+    latest_posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:5]
+    return render(request, 'blog/post_list.html', {'posts': latest_posts})
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, "blog/detail.html", {"post": post})
+
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:detail', post_id=post.id) # kwargs here need to match what is used in urls.py - no magic
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            # post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:detail', post_id=post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+@login_required
+def post_publish(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.publish()
+    return redirect('blog:detail', post_id=post_id)
+
+
+@login_required
+def post_remove(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+    return redirect('blog:post_list')
+
+
+def add_comment_to_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blog:detail', post_id=post.id)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, post_id):
+    comment = get_object_or_404(Comment, pk=post_id)
+    comment.approve()
+    return redirect('blog:detail', post_id=comment.post.id)
+
+@login_required
+def comment_remove(request, post_id):
+    comment = get_object_or_404(Comment, pk=post_id)
+    comment.delete()
+    return redirect('blog:detail', post_id=comment.post.id)
