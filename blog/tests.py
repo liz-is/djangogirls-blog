731c92124c62e2080ace87c6670f50898faa27b1
Liz Ing-Simmons <liz.ing-simmmons@kcl.ac.uk>
2024-03-26 15:26:37 +0000

    linting and  comments for tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,29 +1,20 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from django.utils import timezone
-from django.urls import reverse
-
 import datetime
 
-from .models import Post, Comment
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Comment, Post
 
 # Create your tests here.
 
 # test index/homepage view
 # response if there are no posts?
-# posts that are published are listed
-# posts that are not published are not listed
 # number of comments are correct?
 
-# test that things that should only be shown when logged in are shown correctly
-
-# test detail view of posts
-# posts only shown if published
-# comments only shown if approved (or logged in)
-# test comment approval process?
-
-
 # Test Models ---------------------------------------------------------------------------
+
 
 def create_post(title="A Title", text="here is some text"):
     """
@@ -34,18 +25,19 @@ def create_post(title="A Title", text="here is some text"):
     return Post.objects.create(author=user, title=title, text=text)
 
 
-def create_comment(author = "me", text = "a comment"):
+def create_comment(author="me", text="a comment"):
     """
-    Creates a comment with the given `author` and `text`, that is unapproved. 
+    Creates a comment with the given `author` and `text`, that is unapproved.
     """
     post = create_post()
-    return Comment.objects.create(post = post, author = author, text = text)
+    return Comment.objects.create(post=post, author=author, text=text)
 
 
 class PostModelTests(TestCase):
     """
     Test that publishing a post sets its `published_date` field to today's date.
     """
+
     def test_publish_post(self):
         blog_post = create_post()
         self.assertIsNone(blog_post.published_date)
@@ -64,7 +56,6 @@ class PostModelTests(TestCase):
 
 
 class CommentModelTests(TestCase):
-   
     def test_str(self):
         text = "This is the comment text"
         comment = create_comment(text=text)
@@ -78,6 +69,7 @@ class CommentModelTests(TestCase):
 
 
 # Test Views ---------------------------------------------------------------------------
+
 
 class PostListViewTests(TestCase):
     @classmethod
@@ -160,13 +152,13 @@ class PostListViewTests(TestCase):
 
     def test_draft_posts__require_login(self):
         response = self.client.get(reverse("blog:post_draft_list"))
-        self.assertRedirects(response, '/accounts/login/?next=/drafts/')
+        self.assertRedirects(response, "/accounts/login/?next=/drafts/")
 
     def test_draft_posts_displayed(self):
         """
         Test that unpublished posts are visible to the logged-in user.
         """
-        self.assertTrue(self.client.login(username = "test", password = "secret"))
+        self.assertTrue(self.client.login(username="test", password="secret"))
         response = self.client.get(reverse("blog:post_draft_list"))
         self.assertEqual(response.status_code, 200)
         post_list = response.context["post_list"]
@@ -193,19 +185,22 @@ class PostDetailViewTests(TestCase):
     def test_post_detail_view(self):
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 1}))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['post'], self.published_post)
-        
+        self.assertEqual(response.context["post"], self.published_post)
+
     def test_post_draft_detail_view(self):
+        """
+        Test that logged-out user can't access unpublished posts but logged-in user can.
+        """
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 2}))
         self.assertEqual(response.status_code, 404)
         # test post that doesn't exist yet
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 3}))
         self.assertEqual(response.status_code, 404)
         # log in and try again
-        self.assertTrue(self.client.login(username = "test", password = "secret"))
+        self.assertTrue(self.client.login(username="test", password="secret"))
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 2}))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['post'], self.unpublished_post)
+        self.assertEqual(response.context["post"], self.unpublished_post)
 
 
 class CommentViewTests(TestCase):
@@ -219,25 +214,30 @@ class CommentViewTests(TestCase):
         cls.post.publish()
 
     def test_add_comment_page_exists(self):
-        response = self.client.get(reverse("blog:add_comment_to_post", kwargs={"pk": 1}))
+        response = self.client.get(
+            reverse("blog:add_comment_to_post", kwargs={"pk": 1})
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'blog/add_comment_to_post.html')
+        self.assertTemplateUsed(response, "blog/add_comment_to_post.html")
 
     def test_no_comments_message(self):
+        """
+        Test that if no comments exist or no comments are approved correct message is shown.
+        """
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 1}))
         self.assertContains(response, "No comments here yet :(")
         # create comment and check visibility
         comment_text = "this is my comment text"
-        Comment.objects.create(post = self.post, author = "me", text = comment_text)
+        Comment.objects.create(post=self.post, author="me", text=comment_text)
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 1}))
         self.assertNotContains(response, comment_text)
         self.assertContains(response, "No comments here yet :(")
 
     def test_logged_in_user_comment_visibility(self):
         comment_text = "this is my comment text"
-        comment = Comment.objects.create(post = self.post, author = "me", text = comment_text)
+        comment = Comment.objects.create(post=self.post, author="me", text=comment_text)
         # log in and can see comment
-        self.assertTrue(self.client.login(username = "test", password = "secret"))
+        self.assertTrue(self.client.login(username="test", password="secret"))
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 1}))
         self.assertContains(response, comment_text)
         # can see approved comments too
@@ -247,7 +247,7 @@ class CommentViewTests(TestCase):
 
     def test_logged_out_user_comment_visibility(self):
         comment_text = "this is my comment text"
-        comment = Comment.objects.create(post = self.post, author = "me", text = comment_text)
+        comment = Comment.objects.create(post=self.post, author="me", text=comment_text)
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 1}))
         # can't see comment
         self.assertNotContains(response, comment_text)
@@ -266,15 +266,22 @@ class CommentApproveTests(TestCase):
             author=user, title="A post", text="post text goes here"
         )
         cls.post.publish()
-        cls.comment = Comment.objects.create(post = cls.post, author = "me", text = "A comment")
-    
+        cls.comment = Comment.objects.create(
+            post=cls.post, author="me", text="A comment"
+        )
+
     def test_comment_approve(self):
-        self.assertTrue(self.client.login(username = "test", password = "secret"))
+        """
+        Test that logged-in user can approve comments.
+        """
+        self.assertTrue(self.client.login(username="test", password="secret"))
         detail_response = self.client.get(reverse("blog:detail", kwargs={"pk": 1}))
         # is there a better way to do this?
-        post_id = detail_response.context['post'].id
-        comment = Comment.objects.get(post = post_id)
-        response = self.client.post(reverse("blog:comment_approve", kwargs={"pk": comment.id}))
+        post_id = detail_response.context["post"].id
+        comment = Comment.objects.get(post=post_id)
+        response = self.client.post(
+            reverse("blog:comment_approve", kwargs={"pk": comment.id})
+        )
         self.assertRedirects(response, reverse("blog:detail", kwargs={"pk": post_id}))
         # check that approved comment is now visible when logged out
         self.client.logout()
@@ -282,12 +289,17 @@ class CommentApproveTests(TestCase):
         self.assertContains(response, comment.text)
 
     def test_comment_delete(self):
-        self.assertTrue(self.client.login(username = "test", password = "secret"))
+        """
+        Test that logged-in user can delete comments.
+        """
+        self.assertTrue(self.client.login(username="test", password="secret"))
         detail_response = self.client.get(reverse("blog:detail", kwargs={"pk": 1}))
         # is there a better way to do this?
-        post_id = detail_response.context['post'].id
-        comment = Comment.objects.get(post = post_id)
-        response = self.client.post(reverse("blog:comment_remove", kwargs={"pk": comment.id}))
+        post_id = detail_response.context["post"].id
+        comment = Comment.objects.get(post=post_id)
+        response = self.client.post(
+            reverse("blog:comment_remove", kwargs={"pk": comment.id})
+        )
         self.assertRedirects(response, reverse("blog:detail", kwargs={"pk": post_id}))
         # check that deleted comment is not visible
         response = self.client.get(reverse("blog:detail", kwargs={"pk": 1}))
@@ -296,6 +308,7 @@ class CommentApproveTests(TestCase):
 
 
 # Test Forms ---------------------------------------------------------------------------
+
 
 class PostFormTests(TestCase):
     @classmethod
@@ -308,6 +321,9 @@ class PostFormTests(TestCase):
         cls.post.publish()
 
     def test_only_logged_in_users_can_create_posts(self):
+        """
+        Test that logged-in user can create posts but logged-out user gets redirected to login page.
+        """
         response = self.client.get(reverse("blog:post_new"))
         self.assertRedirects(response, "/accounts/login/?next=/post/new/")
         self.assertTrue(self.client.login(username="test", password="secret"))
@@ -316,6 +332,9 @@ class PostFormTests(TestCase):
         self.assertTemplateUsed(response, "blog/post_edit.html")
 
     def test_only_logged_in_users_can_edit_posts(self):
+        """
+        Test that logged-in user can edit posts but logged-out user gets redirected to login page.
+        """
         post_id = self.post.id
         response = self.client.get(reverse("blog:post_edit", kwargs={"pk": post_id}))
         self.assertRedirects(response, f"/accounts/login/?next=/post/{post_id}/edit/")
@@ -329,35 +348,56 @@ class PostFormTests(TestCase):
         # The message that appears in the browser is actually "Please fill in this field."!
         # But at least this tests whether there's an error or not
         self.assertTrue(self.client.login(username="test", password="secret"))
-        response = self.client.post(reverse("blog:post_new"),
-                                    {"title": "",
-                                     "text": ""})
-        self.assertFormError(response.context['form'], field='title', errors='This field is required.')
-        self.assertFormError(response.context['form'], field='text', errors='This field is required.')
-        
+        response = self.client.post(reverse("blog:post_new"), {"title": "", "text": ""})
+        self.assertFormError(
+            response.context["form"], field="title", errors="This field is required."
+        )
+        self.assertFormError(
+            response.context["form"], field="text", errors="This field is required."
+        )
+
     def test_post_author_is_current_user(self):
         self.assertTrue(self.client.login(username="test", password="secret"))
-        response = self.client.post(reverse("blog:post_new"),
-                                    {"title": "A title",
-                                     "text": "Some text"}, follow=True) # follow redirect
-        self.assertEqual(str(response.context['post'].author), 'test')
+        response = self.client.post(
+            reverse("blog:post_new"),
+            {"title": "A title", "text": "Some text"},
+            follow=True,
+        )  # follow redirect
+        self.assertEqual(str(response.context["post"].author), "test")
         # edit the first post
         post_id = self.post.id
-        response = self.client.post(reverse("blog:post_edit", kwargs={"pk": post_id}),
-                                    {"title": "A title",
-                                     "text": "Some text"}, follow=True)
-        self.assertEqual(str(response.context['post'].author), 'test')
+        response = self.client.post(
+            reverse("blog:post_edit", kwargs={"pk": post_id}),
+            {"title": "A title", "text": "Some text"},
+            follow=True,
+        )
+        self.assertEqual(str(response.context["post"].author), "test")
 
     def test_only_logged_in_users_can_publish(self):
-        new_post = Post.objects.create(author = self.user, title = "title", text = "post text goes here")
-        response = self.client.get(reverse("blog:post_publish", kwargs={"pk": new_post.id}))
-        self.assertRedirects(response, f"/accounts/login/?next=/post/{new_post.id}/publish/")
+        """
+        Test that logged-in user can publish posts but logged-out user gets redirected to login page.
+        """
+        new_post = Post.objects.create(
+            author=self.user, title="title", text="post text goes here"
+        )
+        response = self.client.get(
+            reverse("blog:post_publish", kwargs={"pk": new_post.id})
+        )
+        self.assertRedirects(
+            response, f"/accounts/login/?next=/post/{new_post.id}/publish/"
+        )
         # log in and try again
         self.assertTrue(self.client.login(username="test", password="secret"))
-        response = self.client.get(reverse("blog:post_publish", kwargs={"pk": new_post.id}))
+        response = self.client.get(
+            reverse("blog:post_publish", kwargs={"pk": new_post.id})
+        )
         self.assertRedirects(response, f"/{new_post.id}/")
-        response = self.client.get(reverse("blog:post_publish", kwargs={"pk": new_post.id}), follow=True)
-        self.assertEqual(response.context['post'].published_date.date(), timezone.now().date())
+        response = self.client.get(
+            reverse("blog:post_publish", kwargs={"pk": new_post.id}), follow=True
+        )
+        self.assertEqual(
+            response.context["post"].published_date.date(), timezone.now().date()
+        )
 
 
 class CommentFormTests(TestCase):
@@ -371,17 +411,26 @@ class CommentFormTests(TestCase):
         cls.post.publish()
 
     def test_redirects_to_post_on_success(self):
-        response = self.client.post(reverse("blog:add_comment_to_post", kwargs={"pk": 1}),
-                                    {"author": "me",
-                                     "text": "comment text goes here"})
+        """
+        Test that adding a comment redirects correctly.
+        """
+        response = self.client.post(
+            reverse("blog:add_comment_to_post", kwargs={"pk": 1}),
+            {"author": "me", "text": "comment text goes here"},
+        )
         self.assertRedirects(response, reverse("blog:detail", kwargs={"pk": 1}))
 
     def test_comment_form_invalid_if_empty_fields(self):
         # I'm not sure where the default error message is defined
         # The message that appears in the browser is actually "Please fill in this field."!
         # But at least this tests whether there's an error or not
-        response = self.client.post(reverse("blog:add_comment_to_post", kwargs={"pk": 1}),
-                                    {"author": "",
-                                     "text": ""})
-        self.assertFormError(response.context['form'], field='author', errors='This field is required.')
-        self.assertFormError(response.context['form'], field='text', errors='This field is required.')
+        response = self.client.post(
+            reverse("blog:add_comment_to_post", kwargs={"pk": 1}),
+            {"author": "", "text": ""},
+        )
+        self.assertFormError(
+            response.context["form"], field="author", errors="This field is required."
+        )
+        self.assertFormError(
+            response.context["form"], field="text", errors="This field is required."
+        )
