4c5a92819a2b01b3929cf29d44f47879437358ad
Liz Ing-Simmons <liz.ing-simmons@kcl.ac.uk>
2024-04-19 10:13:36 +0100

    build: trying to get access to site running on VM working

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DJANGO_DEBUG"]
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "10.211.117.115", "djangoblog.sites.er.kcl.ac.uk"]
 
 
 # Application definition
